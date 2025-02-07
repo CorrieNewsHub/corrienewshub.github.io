eb8d283048a135129bcfcacca595527ec1fbdb2d
tdbnz-coding <49893160+tdbnz-coding@users.noreply.github.com>
2025-02-07 22:59:49 +1300

    update posts

--- posts.js
+++ posts.js
@@ -1,44 +1,58 @@
 const posts = [
     {
-        id: 2,
-        title: "Nightmare Neighbours Arrive on Coronation Street",
+        id: 1,
+        title: "Nightmare Neighbours Shake Up Coronation Street",
         author: "Admin",
         image: "https://images.ctfassets.net/ti0n6mm28of1/7KqoTeaFffok7Mu4a81JaN/3208690d894675bdb0d71caa0dddc8a2/EMBARGO_CORO_MICK_LOU.jpg",
         content: `
-         <h3>Nightmare Neighbours Shake Up Coronation Street</h3>
-<p>Residents of Weatherfield will need to watch their backs as Coronation Street welcomes the arrival of new &lsquo;nightmare neighbours&rsquo; this February.</p>
-<p>Mick and Lou Michaelis will arrive on the street all guns blazing, making life hell for anyone who dares to cross them.</p>
-<p>The Michaelis family will be moving into Mawdsley Street, at the back of Chesney and Gemma&rsquo;s house, marking the first time Coronation Street has extended the drama across the ginnel.</p>
-<p>Initially arriving on her own on the 12th February, Lou (<strong>Farrel Hegarty</strong>) immediately sparks up a friendship with Gemma (<strong>Dolly-Rose Campbell</strong>) as they recognise each other from Bessie Street School, where Gemma&rsquo;s quads and Lou&rsquo;s two young daughters attend.</p>
-<p>When Mick (<strong>Joe Layton</strong>) appears on 14th February, the audience will be shocked to realise his true identity&mdash;the man with the silver van who has been harassing Chesney (<strong>Sam Aston</strong>), who is none the wiser to who Mick really is.</p>
-<p>After developing a friendship with the Winter-Browns, it&rsquo;s evident that Mick and Lou are determined to create upheaval, starting with a prior connection to Kit Green (<strong>Jacob Roberts</strong>), leading to them exposing his secretive past.</p>
-<h4>Cast and Crew Speak</h4>
-<h4>Farrel Hegarty on Joining the Cast</h4>
-<p>"I&rsquo;m delighted and so grateful to be joining the cast of Coronation Street. I have genuinely been blown away by how ridiculously warm and nice everyone is."</p>
-<p>"Lou is definitely an exciting character to play because she&rsquo;s got good and bad parts to her, she&rsquo;s a life force and full of beans. She&rsquo;s a very colourful person who likes to make waves and try to cause chaos."</p>
-<h3>Joe Layton on Playing Mick</h3>
-<p>"Coronation Street is such an institute in British television and to get to walk down the cobbles in Mick&rsquo;s shoes is so exciting. To also do it alongside Farrel is awesome&mdash;we are having great fun together and connecting really well whilst developing our characters."</p>
-<h3>Producer Kate Brooks on the New Characters</h3>
-<p>"Chaotic and loud, Mick and Lou bulldoze their way into Weatherfield, causing absolute carnage for the residents of Coronation Street. Their relationship is volatile to say the least, but at the heart of it lies a fierce sense of family loyalty."</p>
-<h4>Q&amp;A With Joe Layton</h4>
-<h4>Welcome to the Cobbles - how are you enjoying your time here?</h4>
-<p>"I absolutely love it! Everyone's really welcoming. It really is everything I'd hoped it would be."</p>
-<h4>How did you get the part?</h4>
-<p>"I initially had to do a self-tape, and then I got called in for a screen test. Farrel and I hit it off immediately, and it just felt right."</p>
-<h4>What makes a soap different from other acting experiences?</h4>
-<p>"The pace is incredible. We shoot twelve scripts out of order with multiple directors. You have to trust your instincts and go for it."</p>
-<h4>Describe Mick in three words.</h4>
-<p>Impulsive, hot-headed, unpredictable.<br /><br />____________________________________</p>
-<h4>Q&amp;A With Farrel Hegarty</h4>
-<h4>The news is finally out&mdash;how are you enjoying your time on Coronation Street?</h4>
-<p>"It's been so much fun! We definitely came in hitting the ground running. It&rsquo;s so nice to come in as a duo&mdash;we're thick as thieves."</p>
-<h4>How did you get the part?</h4>
-<p>"I did a self-tape, then a screen test. The chemistry with Joe during the test was amazing&mdash;we even had a scene where I threw a spam sandwich at his face!"</p>
-<h4>Describe Lou in three words.</h4>
-<p>Irrational, gobby, fiery&mdash;but also loyal.</p>
-<h4>What do you love about Coronation Street?</h4>
-<p>"I&rsquo;ve been blown away by how warm and welcoming everyone is. The cast and crew are like a proper family. It&rsquo;s been an enjoyable experience."</p>
-<p>`,</p>,
-        publishDate: "2025-02-07T15:00:00+13:00"
-   }
+            <h3>Nightmare Neighbours Shake Up Coronation Street</h3>
+            <p>Residents of Weatherfield will need to watch their backs as Coronation Street welcomes the arrival of new &lsquo;nightmare neighbours&rsquo; this February.</p>
+            <p>Mick and Lou Michaelis will arrive on the street all guns blazing, making life hell for anyone who dares to cross them.</p>
+            <p>The Michaelis family will be moving into Mawdsley Street, at the back of Chesney and Gemma&rsquo;s house, marking the first time Coronation Street has extended the drama across the ginnel.</p>
+            <p>Initially arriving on her own on the 12th February, Lou (<strong>Farrel Hegarty</strong>) immediately sparks up a friendship with Gemma (<strong>Dolly-Rose Campbell</strong>) as they recognise each other from Bessie Street School, where Gemma&rsquo;s quads and Lou&rsquo;s two young daughters attend.</p>
+            <p>When Mick (<strong>Joe Layton</strong>) appears on 14th February, the audience will be shocked to realise his true identity&mdash;the man with the silver van who has been harassing Chesney (<strong>Sam Aston</strong>), who is none the wiser to who Mick really is.</p>
+            <p>After developing a friendship with the Winter-Browns, it&rsquo;s evident that Mick and Lou are determined to create upheaval, starting with a prior connection to Kit Green (<strong>Jacob Roberts</strong>), leading to them exposing his secretive past.</p>
+            
+            <h4>Cast and Crew Speak</h4>
+            
+            <h4>Farrel Hegarty on Joining the Cast</h4>
+            <p>"I&rsquo;m delighted and so grateful to be joining the cast of Coronation Street. I have genuinely been blown away by how ridiculously warm and nice everyone is."</p>
+            <p>"Lou is definitely an exciting character to play because she&rsquo;s got good and bad parts to her, she&rsquo;s a life force and full of beans. She&rsquo;s a very colourful person who likes to make waves and try to cause chaos."</p>
+
+            <h3>Joe Layton on Playing Mick</h3>
+            <p>"Coronation Street is such an institute in British television and to get to walk down the cobbles in Mick&rsquo;s shoes is so exciting. To also do it alongside Farrel is awesome&mdash;we are having great fun together and connecting really well whilst developing our characters."</p>
+
+            <h3>Producer Kate Brooks on the New Characters</h3>
+            <p>"Chaotic and loud, Mick and Lou bulldoze their way into Weatherfield, causing absolute carnage for the residents of Coronation Street. Their relationship is volatile to say the least, but at the heart of it lies a fierce sense of family loyalty."</p>
+            
+            <h4>Q&amp;A With Joe Layton</h4>
+            
+            <h4>Welcome to the Cobbles - how are you enjoying your time here?</h4>
+            <p>"I absolutely love it! Everyone's really welcoming. It really is everything I'd hoped it would be."</p>
+
+            <h4>How did you get the part?</h4>
+            <p>"I initially had to do a self-tape, and then I got called in for a screen test. Farrel and I hit it off immediately, and it just felt right."</p>
+
+            <h4>What makes a soap different from other acting experiences?</h4>
+            <p>"The pace is incredible. We shoot twelve scripts out of order with multiple directors. You have to trust your instincts and go for it."</p>
+
+            <h4>Describe Mick in three words.</h4>
+            <p>Impulsive, hot-headed, unpredictable.</p>
+
+            <h4>Q&amp;A With Farrel Hegarty</h4>
+
+            <h4>The news is finally out&mdash;how are you enjoying your time on Coronation Street?</h4>
+            <p>"It's been so much fun! We definitely came in hitting the ground running. It&rsquo;s so nice to come in as a duo&mdash;we're thick as thieves."</p>
+
+            <h4>How did you get the part?</h4>
+            <p>"I did a self-tape, then a screen test. The chemistry with Joe during the test was amazing&mdash;we even had a scene where I threw a spam sandwich at his face!"</p>
+
+            <h4>Describe Lou in three words.</h4>
+            <p>Irrational, gobby, fiery&mdash;but also loyal.</p>
+
+            <h4>What do you love about Coronation Street?</h4>
+            <p>"I&rsquo;ve been blown away by how warm and welcoming everyone is. The cast and crew are like a proper family. It&rsquo;s been an enjoyable experience."</p>
+        `,
+        publishDate: "2025-02-07T22:57:00+13:00"
+    }
 ];
